d85dd0ba19c234af30e220abd0f67a22cb573516
Hieuato <hieuatoatomta3@gmail.com>
2020-08-27 14:54:08 +0700

    asdasd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,9 +5,10 @@
  */
 export const environment = {
   production: true,
-  // api_url: 'http://103.16.1.59:8083/api',
+  api_url: 'http://103.16.1.59:8083/api',
   // url: 'http://103.16.1.59:8001/mic',
+   url: 'https://hieuatomta.github.io/report-web',
 
-  api_url: 'http://localhost:8083/api',
-  url: 'http://localhost:4200',
+  // api_url: 'http://localhost:8083/api',
+  // url: 'http://localhost:4200',
 };
